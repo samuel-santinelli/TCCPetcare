c39a0054f993d1b3ac94c0d039ae73e46f9b9052
jonatas <jonatassantos152409@gmail.com>
2022-04-19 10:59:55 -0300

    implementacao do login

--- Cliente/autenticacao.php
+++ Cliente/autenticacao.php
@@ -1,37 +1,43 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: token, Content-Type');
+header('Access-Control-Max-Age: 1728000');
+header('Content-Length: 0');
+header('Content-Type: text/plain');
+
 require_once("config/config.php");
 require_once(SRC."bd/conexao.php");
 
-$email = (string) null; 
+$login = (string) null; 
 $senha = (string) null;
 
-$email = $_POST['email']; 
+$login = $_POST['login']; 
 $senha = md5($_POST['senha']); 
 
-function login($email, $senha){
-    
-if($email != "" && $senha != ""){ 
-    $sql = "select *from tblClientes where email ='".$email."' and
+
+if($login != "" && $senha != ""){ // 5 passo, fazendo o tratamento 
+    $sql = "select *from tblClientes where email ='".$login."' and
     senha='".$senha."'";
 
-    $conexao =  conexao(); 
+    $conexao =  conexao(); //6 passo, chamando a funcao
 
-    // echo($sql);
-    // die;
+    echo($sql);
+    die;
     $select = mysqli_query($conexao,$sql);
 
-   if($exibirCLientes= mysqli_fetch_assoc($select)){
-    session_start(); 
+   if($exibirUsuarios= mysqli_fetch_assoc($select)){
+    session_start();
 
     $_SESSION['statusLogin'] = true;
-    $_SESSION['nome'] = $exibirCLientes['nome'];
-      header('location:indexPrincipal.php');
+    $_SESSION['nomeUsuario'] = $exibirUsuarios['nome'];
+      header('location:indexDash.php');
     // echo('foi');
     }else{
-        echo("Senha invalida");
+        echo('LOGIN_MSG_INVALIDO');
     }
 }
-}
+
 
 ?>
--- Cliente/index.php
+++ Cliente/index.php
@@ -1,25 +1,55 @@
+<?php
+    session_start();
+
+
+
+?>
 
 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
 </head>
 <body>
-
-    <form action="autenticacao.php" method="POST">
-    
-    <label>Email:</label>
-    <input value="" placeholder="email" type="email" name="email"/>
-
-    <label>Senha:</label>
-    <input value="" placeholder="senha" type="password" name="senha"/>      
-
-    <button type="submit">Entrar</button>
-
-    </form>
+<div id="cadastro"> 
+            <div id="cadastroTitulo"> 
+                <h1> Cadastro de Usuarios </h1>
+            </div>
+            <div id="cadastroInformacoes">
+        
+                <form action="autenticar.php" name="frmCadastro" method="post" >
+                   
+                    <div class="campo">
+                        <div class="informacoesPessoais">
+                            <label> Login: </label>
+                        </div>
+                        <div class="entradaDeDados">
+                            <input type="email" name="login" value="" placeholder="Digite seu Login" maxlength="100">
+                        </div>
+                    </div>
+                    <div class="campo">
+                        <div class="informacoesPessoais">
+                            <label> Senha: </label>
+                        </div>
+                        <div class="entradaDeDados">
+                            <input type="password" name="senha" value="" placeholder="Digite sua senha" maxlength="100">
+                        </div>
+                    </div>
+                
+                    
+                    <div class="enviar">
+                        <div class="enviar">
+                            <input type="submit" name="btnEnviar" value="Autenticar">
+                        </div>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
 </body>
 </html>
