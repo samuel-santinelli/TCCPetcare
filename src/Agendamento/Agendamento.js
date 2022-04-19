8e15c08bd173f7d9cd321aa667b52fcd14373bae
samuel-santinelli <samuelsantinelli@gmail.com>
2022-04-19 16:03:18 -0300

    arrumando a estilizacao do consumo

--- src/Agendamento/Agendamento.js
+++ src/Agendamento/Agendamento.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import SearchIcon from "@mui/icons-material/Search";
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
+import { RiBookmarkLine } from "react-icons/ri";
+import { MdOutlineStarOutline, MdOutlineStarPurple500, MdOutlineStarHalf } from "react-icons/md";
 import StarIcon from "@mui/icons-material/Star";
 import "./style/styleAgendamento.css";
 import ButtonAgendar from "./button/ButtonAgendar";
@@ -59,17 +61,18 @@ const USERDATA = [
 ];
 
 const Agendamento = () => {
-    const [cuidadores, setCuidadores] = useState([]);
-  
-    useEffect(() => {
-      axios
-        .get("http://localhost/Cuidador/Cuidador/api/cuidador")
-        .then((res) => {
-           setCuidadores(res.data)
-          }).catch(() => {
-            console.log("Deu erro")
-          }) 
-        }, [])
+  const [cuidadores, setCuidadores] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost/Cuidador/Cuidador/api/cuidador")
+      .then((res) => {
+        setCuidadores(res.data);
+      })
+      .catch(() => {
+        console.log("Deu erro");
+      });
+  }, []);
 
   return (
     <div className="containerMain">
@@ -95,26 +98,23 @@ const Agendamento = () => {
                     <div className="imageCuidador" />
                     <div className="infoCuidador">
                       <label className="nameCuidador">{cuidador.nome}</label>
-                      <label className="hourCuidador">{cuidador.limitacoes}</label>
+                      <label className="hourCuidador">
+                        {cuidador.limitacoes}
+                      </label>
                       <div className="containerRating">
-                        <Rating
-                          name="text-feedback"
-                          readOnly
-                          precision={0.5}
-                          emptyIcon={
-                            <StarIcon
-                              style={{ opacity: 0.95 }}
-                              fontSize="inherit"
-                            />
-                          }
-                        />
+                      <MdOutlineStarPurple500 className="iconStar"/>
+                      <MdOutlineStarPurple500 className="iconStar"/>
+                      <MdOutlineStarPurple500 className="iconStar"/>
+                      <MdOutlineStarHalf className="iconStar"/>
                       </div>
                       <div className="containerBio">
                         <label className="biographyCuidador">
                           {cuidador.biografia}
                         </label>
                       </div>
+                      
                       <div className="containerMap">
+                      <RiBookmarkLine className="iconInputSave" />
                         <iframe
                           className="localMap"
                           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29276.363111143826!2d-46.88371138889647!3d-23.4768574223817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf03aaf6d062af%3A0x2c22de58cd7f17f1!2sAlphaville%2C%20Santana%20de%20Parna%C3%ADba%20-%20SP%2C%2006542-115!5e0!3m2!1spt-BR!2sbr!4v1640096190707!5m2!1spt-BR!2sbr"
@@ -127,82 +127,6 @@ const Agendamento = () => {
                   </div>
                 </div>
               ))}
-              {/* <div className="card">
-                <div className="contentCenterCuidador">
-                  <div className="imageCuidador" />
-                  <div className="infoCuidador">
-                    <label className="nameCuidador">Fernanda Silva</label>
-                    <label className="hourCuidador">Ontem as 09:35</label>
-                    <div className="containerRating">
-                      <Rating
-                        name="text-feedback"
-                        readOnly
-                        precision={0.5}
-                        emptyIcon={
-                          <StarIcon
-                            style={{ opacity: 0.95 }}
-                            fontSize="inherit"
-                          />
-                        }
-                      />
-                    </div>
-                    <div className="containerBio">
-                      <label className="biographyCuidador">
-                        Minha experiência com animais começou no ano de 2009,
-                        quando comecei a cuidar de um gato da minha vizinha, que
-                        por motivo de trabalho...
-                      </label>
-                    </div>
-                    <div className="containerMap">
-                      <iframe
-                        listElementsCuidadores="localMap"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29276.363111143826!2d-46.88371138889647!3d-23.4768574223817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf03aaf6d062af%3A0x2c22de58cd7f17f1!2sAlphaville%2C%20Santana%20de%20Parna%C3%ADba%20-%20SP%2C%2006542-115!5e0!3m2!1spt-BR!2sbr!4v1640096190707!5m2!1spt-BR!2sbr"
-                        allowfullscreen=""
-                        loading="lazy"
-                      ></iframe>
-                    </div>
-                    <ButtonAgendar />
-                  </div>
-                </div>
-              </div> */}
-              {/* <div className="card">
-                <div className="contentCenterCuidador">
-                  <div className="imageCuidador" />
-                  <div className="infoCuidador">
-                    <label className="nameCuidador">Fernanda Silva</label>
-                    <label className="hourCuidador">Ontem as 09:35</label>
-                    <div className="containerRating">
-                      <Rating
-                        name="text-feedback"
-                        readOnly
-                        precision={0.5}
-                        emptyIcon={
-                          <StarIcon
-                            style={{ opacity: 0.95 }}
-                            fontSize="inherit"
-                          />
-                        }
-                      />
-                    </div>
-                    <div className="containerBio">
-                      <label className="biographyCuidador">
-                        Minha experiência com animais começou no ano de 2009,
-                        quando comecei a cuidar de um gato da minha vizinha, que
-                        por motivo de trabalho...
-                      </label>
-                    </div>
-                    <div className="containerMap">
-                      <iframe
-                        class="localMap"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29276.363111143826!2d-46.88371138889647!3d-23.4768574223817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf03aaf6d062af%3A0x2c22de58cd7f17f1!2sAlphaville%2C%20Santana%20de%20Parna%C3%ADba%20-%20SP%2C%2006542-115!5e0!3m2!1spt-BR!2sbr!4v1640096190707!5m2!1spt-BR!2sbr"
-                        allowfullscreen=""
-                        loading="lazy"
-                      ></iframe>
-                    </div>
-                    <ButtonAgendar />
-                  </div>
-                </div>
-              </div> */}
             </div>
 
             {/* Conteudo da modal de cuidadores */}
