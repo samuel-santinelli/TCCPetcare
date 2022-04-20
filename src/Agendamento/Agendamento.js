25f472cc09d5bab49e6ab2e301805783d598d519
samuel.santinelli <samuelsantinelli@gmail.com>
2022-04-20 13:28:50 -0300

    estilizacao do consumo pronta

--- src/Agendamento/Agendamento.js
+++ src/Agendamento/Agendamento.js
@@ -50,7 +50,7 @@ const Agendamento = () => {
                     <div className="infoCuidador">
                       <label className="nameCuidador">{cuidador.nome}</label>
                       <label className="hourCuidador">
-                        {cuidador.limitacoes}
+                        {cuidador.moradia}
                       </label>
                       <div className="containerRating">
                       <MdOutlineStarPurple500 className="iconStar"/>
@@ -66,7 +66,7 @@ const Agendamento = () => {
                       <ButtonAgendar />
                       </div>
                       <div className="containerMap">
-                      <RiBookmarkLine className="iconInputSave" />
+                      {/* <RiBookmarkLine className="iconInputSave" /> */}
                         <iframe
                           className="localMap"
                           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29276.363111143826!2d-46.88371138889647!3d-23.4768574223817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf03aaf6d062af%3A0x2c22de58cd7f17f1!2sAlphaville%2C%20Santana%20de%20Parna%C3%ADba%20-%20SP%2C%2006542-115!5e0!3m2!1spt-BR!2sbr!4v1640096190707!5m2!1spt-BR!2sbr"
