ed38379db24667f8c4f2a38b8e81a31c76381fe5
samuel-santinelli <samuelsantinelli@gmail.com>
2022-05-08 11:45:31 -0300

    Arrumando os headers

---
+++ Cuidador/Cliente/api/index.php
@@ -0,0 +1,39 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: Content-Type');
+        header('Content-Type: application/json');
+   
+require_once("../config/config.php");
+   
+$url = (string) null;
+   
+       
+$url = explode('/',$_GET['url']); 
+           
+       switch($url[0]){
+           case 'cliente':
+                    require_once('clienteApi/index.php');
+                    break;  
+            case 'pets':
+                    require_once('petsApi/index.php');
+                    break; 
+            case 'comportamento':
+                    require_once('comportamentoApi/index.php');
+                    break;   
+            case 'vacinas':
+                    require_once('vacinaApi/index.php');
+                    break;   
+        case 'raca':
+                require_once('racaApi/index.php');
+                break;  
+                case 'servicoHost':
+                        require_once('servicoApi/index.php');
+                        break;  
+}
+   
+    
+    
+
+?>
---
+++ Cuidador/Cuidador/api/index.php
@@ -0,0 +1,31 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: *');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Content-Type: application/json');
+
+     require_once("../config/config.php");
+
+    $url = (string) null;
+
+    
+    $url = explode('/',$_GET['url']); 
+        
+    switch($url[0]){
+        case 'cuidador':
+                 require_once('cuidadorApi/index.php');
+                 break;  
+                 case 'agendar':
+                    require_once('agendamentoApi/index.php');
+                    break;       
+                    case 'historico':
+                        require_once('historicoHost/index.php');
+                        break;         
+
+    }
+
+    
+    
+
+
+?>
