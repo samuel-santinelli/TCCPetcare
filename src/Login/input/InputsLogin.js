00c5fa822c6cea0946b725d716006d4fae20dfcf
samuel.santinelli <samuelsantinelli@gmail.com>
2022-05-17 14:41:58 -0300

    tela de login

--- src/Login/input/InputsLogin.js
+++ src/Login/input/InputsLogin.js
@@ -1,68 +1,78 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import LockIcon from "@mui/icons-material/Lock";
 import "./style/ContainerInputsLogin.css";
 
-const InputsLogin = () =>{
-  
+const InputsLogin = () => {
+  const navigate = useNavigate();
 
   const [userLogin, setUserLogin] = useState({
     email: "",
-    senha: ""
+    senha: "",
   });
 
   const handleLoginSubmit = (userLogin) => {
-    const email = document.getElementById("emailLogin");
-    const senha = document.getElementById("senhaLogin");
-    
-    axios
-      .get(`http://localhost/Cuidador/Cliente/api/cliente/login?${email}&${senha}`, {
-      email: userLogin.email,
-      senha: userLogin.senha,
-      }).then((res) => {
-        console.log(res.data)
-        
-    })
+    const email = document.getElementById("emailLogin").value
+    const senha = document.getElementById("senhaLogin").value
+
+    if (email === email) {      
+      axios
+      .get(
+        `http://localhost/Cuidador/Cliente/api/cliente/login?email=${email}&senha=${senha}`,
+        {
+          email: email,
+          senha: senha
+        }
+      )
+      .then((res) => {
+        console.log("O cuidador é", res.data);
+      }); 
+    } else {
+      console.log("ta errado irmao");
+    }
   };
-
-
   return (
     <>
-    <form onSubmit={handleLoginSubmit(userLogin)}>
-      <div className="containerInputsLogin">
-        <div id="InputsLogin">
-          <AccountCircle id="iconInputLabelLogin" />
-          <input
-            label="Email"
-            id="emailLogin"
-            className="inputLogin"
-            placeholder="Digite seu email"
-            value={userLogin.email}
-            onChange={(e) => setUserLogin({...userLogin, email: e.target.value})}
-          />
+      <form onSubmit={handleLoginSubmit(userLogin)}>
+        <div className="containerInputsLogin">
+          <div id="InputsLogin">
+            <AccountCircle id="iconInputLabelLogin" />
+            <input
+              label="Email"
+              id="emailLogin"
+              className="inputLogin"
+              placeholder="Digite seu email"
+              value={userLogin.email}
+              onChange={(e) =>
+                setUserLogin({ ...userLogin, email: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <LockIcon id="iconInputLabelLogin" />
+            <input
+              label="Senha"
+              type="password"
+              id="senhaLogin"
+              className="inputLogin"
+              placeholder="Digite sua senha"
+              value={userLogin.senha}
+              onChange={(e) =>
+                setUserLogin({ ...userLogin, senha: e.target.value })
+              }
+            />
+          </div>
+          <div id="containerButton">
+            <input
+              value="Entre Agora"
+              type="submit"
+              id="buttonProximo"
+              className="button"
+            />
+          </div>
         </div>
-        <div>
-          <LockIcon id="iconInputLabelLogin" />
-          <input
-            label="Senha"
-            type="password"
-            id="senhaLogin"
-            className="inputLogin"
-            placeholder="Digite sua senha"
-            value={userLogin.senha}
-            onChange={(e) => setUserLogin({...userLogin, senha: e.target.value})}
-          />
-        </div>
-        <div id="containerButton">
-          <input
-            value="Entre Agora"
-            type="submit"
-            id="buttonProximo"
-            className="button"
-          />
-        </div>
-      </div>
       </form>
     </>
   );
